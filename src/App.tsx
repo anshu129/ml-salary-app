3525800547162b6bea2395f8455b992a14010899
anshu129 <114096201+anshu129@users.noreply.github.com>
2024-05-20 13:25:53 +0530

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import MainTable from './components/MainTable';
 //import DetailsTable from '/Users/anshu/ml-salary-app/src/components/DeatilsTable';
 import { loadCSVData,  SalaryData, } from './utils/dataLoader';
 import './App.css';
+import { Table, Button } from 'antd';
+import { parse } from 'csv-parse/browser/esm';
 
 const { Header, Content } = Layout;
 const { Option } = Select;
@@ -15,6 +17,63 @@ const App: React.FC = () => {
   //const [rawData, setRawData] = useState<any[]>([]); // Raw data to use for job details
   const [selectedYear, setSelectedYear] = useState<number | null>(null);
   //const [details, setDetails] = useState<JobDetail[]>([]);
+
+  const [dataSource, setDataSource] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch('/salaries.csv')
+      .then((response) => response.text())
+      .then((data) => {
+        parse(data, {
+          columns: true,
+          skip_empty_lines: true,
+        }, (err, output: any[]) => {
+          if (err) {
+            console.error('Error parsing CSV:', err);
+          } else {
+            const jobCounts: { [key: string]: number } = {};
+            output.forEach((item) => {
+              const { job_title } = item;
+              if (jobCounts[job_title]) {
+                jobCounts[job_title]++;
+              } else {
+                jobCounts[job_title] = 1;
+              
+          }
+        });
+        const dataSourceWithJobCounts = output.map((item) => ({
+          ...item,
+          total_jobs: jobCounts[item.job_title],
+        }));
+        setDataSource(dataSourceWithJobCounts);
+      }
+    });
+  })
+  .catch((error) => console.error('Error fetching CSV:', error));
+  }, []);
+
+  const columns = [
+    {
+      title: 'Work Year',
+      dataIndex: 'work_year',
+      sorter: (a: any, b: any) => a.work_year - b.work_year,
+    },
+    {
+      title: 'Job Title',
+      dataIndex: 'job_title',
+      sorter: (a: any, b: any) => a.job_title.localeCompare(b.job_title),
+    },
+    {
+      title: 'Salary in USD',
+      dataIndex: 'salary_in_usd',
+      sorter: (a: any, b: any) => a.salary_in_usd - b.salary_in_usd,
+    },
+    {
+      title: 'Total Jobs',
+      dataIndex: 'total_jobs',
+      sorter: (a: any, b: any) => a.total_jobs - b.total_jobs,
+    },
+  ];
 
   useEffect(() => {
     const fetchData = async () => {
@@ -55,10 +114,13 @@ const App: React.FC = () => {
 
 return (
   <Layout>
-    <Header className="header">
+    <Header className="header" style={{backgroundColor: 'pink'}}>
       <div className="logo" />
       <h1>Salary Data Dashboard</h1>
     </Header>
+    <Content style={{ padding: '0 50px' }} >
+    <Table dataSource={dataSource} columns={columns} rowKey="id" />
+    </Content>
     <Content style={{ padding: '0 50px' }}>
       <div className="site-layout-content">
         <Select
